ede4760cede9863886096cf292f3728e5d52bc6c
jackson <Jackson Hyde>
2024-03-20 12:12:19 +0000

    Untracking dist, post install clean and build

--- dist/TestComponent/TestComponent.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface ITestComponent {
-    severity?: string;
-    content?: string;
-    onClose?: () => void;
-}
-export declare const TestComponent: ({ content, severity, onClose, }: ITestComponent) => React.ReactElement<ITestComponent>;
