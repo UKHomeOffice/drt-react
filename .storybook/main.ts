dcbbaa60d41bfdab2f6477824a5ceb0256993556
Jackson Hyde <jacksonjhyde@gmail.com>
2024-06-21 15:02:40 +0100

    Adding a11y addon (#12)
    Co-authored-by: jackson <Jackson Hyde>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,30 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    '../src/**/*.stories.mdx', 
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
 
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     '@storybook/icons',
     '@storybook/addon-jest',
     '@storybook/addon-themes',
-    "@storybook/addon-webpack5-compiler-babel"
+    '@storybook/addon-webpack5-compiler-babel',
+    '@storybook/addon-a11y',
   ],
 
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
 
   docs: {},
 
   typescript: {
-    reactDocgen: "react-docgen-typescript"
+    reactDocgen: 'react-docgen-typescript'
   }
 };
 export default config;
