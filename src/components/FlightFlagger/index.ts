3c6a6d1ea1c12696a9ae30763ddf7eb7ae7fdd20
jackson <Jackson Hyde>
2024-07-16 10:27:31 +0100

    Flight flagger amends

--- src/components/FlightFlagger/index.ts
+++ src/components/FlightFlagger/index.ts
@@ -2,7 +2,7 @@ export { default } from "./FlightFlagger";
 export type {IFlightFlagger} from './FlightFlagger'
 
 export {FlightFlaggerFilters} from "./FlightFlaggerFilters";
-export type {IFlightFlaggerFilters} from './FlightFlaggerFilters';
+export type {IFlightFlaggerFilters, AutocompleteOption} from './FlightFlaggerFilters';
 
 export {FlightFlaggerResults} from './FlightFlaggerResults';
 export type {IFlightFlaggerResults} from './FlightFlaggerResults';
