2c176c726f3fe8d3113d11cd4b30d5312e53c992
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-08-28 13:10:03 +0100

    Adding min staff form for export
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- src/components/Staff/index.ts
+++ src/components/Staff/index.ts
@@ -1,2 +1,5 @@
 export {MinStaffWarning} from './MinStaffWarning'
+export {MinStaffForm} from './MinStaffForm'
 export type {IMinStaffWarning} from './MinStaffWarning'
+export type {IMinStaffForm} from './MinStaffForm'
+
