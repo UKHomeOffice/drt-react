3dfa0ade929dd9ade8ccabeb8eaa26fba1025bcc
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-09-05 11:32:25 +0100

    Update index for export IMinSuccess Type
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- src/components/Staff/index.ts
+++ src/components/Staff/index.ts
@@ -1,5 +1,7 @@
 export {MinStaffWarning} from './MinStaffWarning'
 export {MinStaffForm} from './MinStaffForm'
+export {MinStaffSuccess} from './MinStaffSuccess'
+export type {IMinStaffSuccess} from './MinStaffSuccess'
 export type {IMinStaffWarning} from './MinStaffWarning'
 export type {IMinStaffForm} from './MinStaffForm'
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,13 @@
-export { default as FlightFlagger } from "./FlightFlagger";
+export {default as FlightFlagger} from "./FlightFlagger";
 
-export { FlightFlaggerFilters, FlightHighlightChip } from "./FlightFlagger";
-export type { IFlightFlagger, IFlightFlaggerFilters, IFlightHighlightChip } from "./FlightFlagger";
+export {FlightFlaggerFilters, FlightHighlightChip} from "./FlightFlagger";
+export type {IFlightFlagger, IFlightFlaggerFilters, IFlightHighlightChip} from "./FlightFlagger";
 
-export { StatusTag } from "./StatusTags";
-export type { IStatusTag } from "./StatusTags";
+export {StatusTag} from "./StatusTags";
+export type {IStatusTag} from "./StatusTags";
 
-export { default as Header } from './Header'
-export type { IHeader, MenuItem } from './Header';
+export {default as Header} from './Header'
+export type {IHeader, MenuItem} from './Header';
 
-export { MinStaffWarning, MinStaffForm } from './Staff'
-export type { IMinStaffWarning, IMinStaffForm } from './Staff'
+export {MinStaffSuccess, MinStaffWarning, MinStaffForm} from './Staff'
+export type {IMinStaffSuccess, IMinStaffWarning, IMinStaffForm} from './Staff'
