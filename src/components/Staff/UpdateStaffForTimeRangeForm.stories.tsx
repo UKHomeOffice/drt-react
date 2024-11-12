8a39db6e6cd68799e5323d226342e9df16ecdf31
Nilesh Gupta <guptanilesh13@gmail.com>
2024-11-12 22:14:37 +0000

    Drtii 1681 time fix (#47)
    * removing time picker and choose specfic interval minutes
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * chaning the way time is used in updateStaff
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * fixing bug for endTime
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * update staffing review comments
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * DRTII-1681 Add 15, 30 & 60 minute stories (#48)
    
    * update endtimeAt midnight
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * adjust end time by a minute
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * remove moment
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * adding explicit day of week 1
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * adding minute back if receiving reduced minute
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * revert the time minus min
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * remove addMinuteIfReduced
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * update data testId
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * move data-testid to Datepicker rather than Texfield slot
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * remove unwanted test and add data it to textfield
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * update testid to cy
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    * adding input props role
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    
    ---------
    
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>
    Co-authored-by: Rich Birch <richbirch@users.noreply.github.com>

--- src/components/Staff/UpdateStaffForTimeRangeForm.stories.tsx
+++ src/components/Staff/UpdateStaffForTimeRangeForm.stories.tsx
@@ -1,16 +1,16 @@
-import moment from 'moment';
-import type {Meta, StoryObj} from '@storybook/react';
+import moment from 'moment'
+import type {Meta, StoryObj} from '@storybook/react'
 import type {IUpdateStaffForTimeRangeData} from './UpdateStaffForTimeRangeForm'
 import {UpdateStaffForTimeRangeForm as UpdateStaffForTimeRangeFormComponent} from './UpdateStaffForTimeRangeForm'
 
 const meta: Meta<typeof UpdateStaffForTimeRangeFormComponent> = {
   title: "DRT Components/UpdateStaffForTimeRangeFormComponent",
   component: UpdateStaffForTimeRangeFormComponent,
-};
+}
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof UpdateStaffForTimeRangeFormComponent>;
+type Story = StoryObj<typeof UpdateStaffForTimeRangeFormComponent>
 
 const ustdForm: IUpdateStaffForTimeRangeData = {
   actualStaff: 1,
@@ -20,15 +20,21 @@ const ustdForm: IUpdateStaffForTimeRangeData = {
   endDayAt: moment().add(1, 'day')
 }
 
-export const UpdateStaffForTimeRangeForm = {
-  args: {
-    ustd: ustdForm,
-    interval: 15,
-    handleSubmit: (essf: IUpdateStaffForTimeRangeData) => {
-      console.log('Submit clicked', essf);
-    },
-    cancelHandler: () => {
-      console.log('Cancel clicked');
-    },
+const UpdateStaffForTimeRangeForm = (interval: number) => {
+  return {
+    args: {
+      ustd: ustdForm,
+      interval: interval,
+      handleSubmit: (essf: IUpdateStaffForTimeRangeData) => {
+        console.log('Submit clicked', essf)
+      },
+      cancelHandler: () => {
+        console.log('Cancel clicked')
+      },
+    }
   }
-};
+}
+
+export const UpdateStaffForTimeRangeForm15: Story = UpdateStaffForTimeRangeForm(15)
+export const UpdateStaffForTimeRangeForm30: Story = UpdateStaffForTimeRangeForm(30)
+export const UpdateStaffForTimeRangeForm60: Story = UpdateStaffForTimeRangeForm(60)
--- src/components/Staff/UpdateStaffForTimeRangeForm.tsx
+++ src/components/Staff/UpdateStaffForTimeRangeForm.tsx
@@ -1,14 +1,19 @@
 import React, {useState} from "react";
-import {Box, Button, IconButton, TextField, TextFieldProps, Typography} from "@mui/material";
+import {Box, Button, IconButton, MenuItem, Select, TextField, Typography} from "@mui/material";
 import moment, {Moment} from "moment";
-import {TimePicker} from '@mui/x-date-pickers/TimePicker';
-import {DatePicker} from '@mui/x-date-pickers/DatePicker';
-import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import CloseIcon from "@mui/icons-material/Close";
-import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PeopleIcon from '@mui/icons-material/People';
+import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+
+moment.updateLocale('en-gb', {
+  week: {
+    dow: 1, // Set the first day of the week to Monday
+  },
+});
 
 export type IUpdateStaffForTimeRangeData = {
   startDayAt: Moment,
@@ -25,6 +30,7 @@ export interface IUpdateStaffForTimeRangeForm {
   cancelHandler: () => void
 }
 
+
 export const UpdateStaffForTimeRangeForm = ({
                                               ustd,
                                               interval,
@@ -33,10 +39,11 @@ export const UpdateStaffForTimeRangeForm = ({
                                             }: IUpdateStaffForTimeRangeForm) => {
   const [startDate, setStartDate] = useState<Moment>(ustd.startDayAt);
   const [startTime, setStartTime] = useState<Moment>(ustd.startTimeAt.startOf('day'));
-  const [endTime, setEndTime] = useState<Moment>(ustd.endTimeAt.startOf('day'));
+  const [endTime, setEndTime] = useState<Moment>(ustd.endTimeAt.startOf('day').add(interval, 'minutes'));
   const [endDate, setEndDate] = useState<Moment>(ustd.endDayAt);
   const [staffNumber, setStaffNumber] = useState<number>(ustd.actualStaff);
   const [error, setError] = useState<string | null>(null);
+
   const handleStartDateChange = (date: Moment | null) => {
     setStartDate(date?.startOf('day') || moment());
     setEndDate(date?.startOf('day') || moment());
@@ -46,18 +53,26 @@ export const UpdateStaffForTimeRangeForm = ({
     setEndDate(date?.startOf('day') || moment());
   };
 
-  const handleStartTimeChange = (date: Moment | null) => {
-    setStartTime(date?.startOf('minute') || moment());
-    if (date && endTime && date.isAfter(endTime)) {
+  const handleStartTimeChange = (hour: number, minute: number) => {
+    const newStartTime = moment(startTime).set({hour, minute});
+    setStartTime(newStartTime);
+    if (newStartTime.isSameOrAfter(endTime)) {
       setError("Start time must be less than or equal to end time.");
       return;
     }
     setError(null);
   };
 
-  const handleEndTimeChange = (date: Moment | null) => {
-    setEndTime(date?.startOf('minute') || moment());
-    if (date && startTime && date.isBefore(startTime)) {
+
+  const handleEndTimeChange = (hour: number, minute: number) => {
+    let newEndTime: Moment;
+    if (startTime.hour() === 0 && startTime.minute() === 0 && hour === 0 && minute === 0) {
+      newEndTime = moment(startTime).add(1, 'day').startOf('day');
+    } else {
+      newEndTime = moment(startTime).set({ hour: hour, minute: minute });
+    }
+    setEndTime(newEndTime);
+    if (newEndTime.isSameOrBefore(startTime)) {
       setError("End time must be greater than or equal to start time.");
       return;
     }
@@ -68,142 +83,186 @@ export const UpdateStaffForTimeRangeForm = ({
     setStaffNumber(event.target.value as number);
   };
 
-  const handleSubmitForm = (event: React.ChangeEvent<{ value: unknown }>) => {
-    const diffInMinutes = (endTime.valueOf() - startTime.valueOf()) / 60000;
-    console.log("endTime.valueOf", endTime.valueOf())
-    console.log("startTime.valueOf", startTime.valueOf())
-    console.log("selectedDate", startDate.valueOf())
-    console.log("diffInMinutes", diffInMinutes)
-    console.log("staffNumber", staffNumber)
+  const handleSubmitForm = () => {
+    const adjustedEndTime = endTime.subtract(1, 'minute');
+
     const ess: IUpdateStaffForTimeRangeData = {
       startDayAt: startDate,
       startTimeAt: startTime,
-      endTimeAt: endTime,
+      endTimeAt: adjustedEndTime,
       endDayAt: endDate,
       actualStaff: staffNumber
     };
     handleSubmit(ess);
+  };
+
+  function timesBy15Minutes(): string[] {
+    const times: string[] = [];
+    for (let hour = 0; hour < 24; hour++) {
+      for (let minute = 0; minute <= 45; minute += interval) {
+        times.push(`${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`);
+      }
+    }
+    return times;
   }
-  const duration = moment.duration(endTime.diff(startTime));
-  const hours = Math.floor(duration.asHours());
-  const minutes = duration.minutes();
 
-  return <Box data-testid={`shift-staff-form`}
-              sx={{
-                paddingTop: '10px',
-                paddingLeft: '20px',
-                paddingBottom: '10px',
-                paddingRight: '20px',
-                width: '400px'
-              }}>
-    <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
-      <Typography variant="h2" component="h2" fontWeight={"bold"}>Edit staff</Typography>
-      <IconButton
-        aria-label="close"
-        color="inherit"
-        size="small"
-        onClick={cancelHandler}>
-        <CloseIcon fontSize="inherit"/>
-      </IconButton>
-    </Box>
-    <Box sx={{paddingTop: '10px'}}>
-      <Typography variant="h3" component="h3">Date</Typography>
-      <Box sx={{paddingTop: '10px'}}>
-        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'en-gb'}>
-          <DatePicker sx={{backgroundColor: '#FFFFFF', width: '100%'}} label="Start Date" value={startDate}
-                      onChange={handleStartDateChange}
-                      format="DD MMMM YYYY"
-                      slots={{ textField: (params) => <TextField {...params} /> }}
-          />
-        </LocalizationProvider>
-      </Box>
-      <Box sx={{paddingTop: '10px'}}>
-        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'en-gb'}>
-          <DatePicker sx={{backgroundColor: '#FFFFFF', width: '100%'}}
-                      label="End Date"
-                      value={endDate}
-                      onChange={handleEndDateChange}
-                      format="DD MMMM YYYY"
-                      slots={{ textField: (params) => <TextField {...params} /> }}
-          />
-        </LocalizationProvider>
-      </Box>
-    </Box>
-    <Box sx={{paddingTop: '10px'}}>
-      <Typography variant="h3" component="h3">Time</Typography>
-      <Box sx={{paddingTop: '10px', display: 'flex', justifyContent: 'flex-start'}}>
-        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'en-gb'}>
-          <TimePicker sx={{backgroundColor: '#FFFFFF', width: '50%'}} label="Start Time"
-                      value={startTime}
-                      ampm={false}
-                      onChange={handleStartTimeChange}
-                      minutesStep={interval}
-                      views={['hours', 'minutes']}
-          />
-        </LocalizationProvider>
-        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'en-gb'}>
-          <TimePicker sx={{paddingLeft: '10px', backgroundColor: '#FFFFFF', width: '50%'}} label="End Time"
-                      value={endTime}
-                      onChange={handleEndTimeChange}
-                      minutesStep={interval}
-                      ampm={false}
-                      views={['hours', 'minutes']}/>
-        </LocalizationProvider>
-      </Box>
-    </Box>
-    <Box sx={{paddingTop: '10px', paddingBottom: '10px'}}>
-      <Typography variant="h3" component="h3">Staff</Typography>
-      <Box sx={{paddingTop: '10px'}}>
-        <TextField sx={{backgroundColor: '#FFFFFF', width: '100%'}} label="Staff Number"
-                   value={staffNumber}
-                   onChange={handleStaffNumberChange} type="number"/>
-      </Box>
-    </Box>
-    {error && <Typography color="error" sx={{paddingTop: '10px'}}>{error}</Typography>}
-    <Box sx={{
-      paddingTop: '10px',
-      paddingBottom: '10px',
-      paddingLeft: '10px',
-      paddingRight: '20px',
-      backgroundColor: '#E6E9F1'
-    }}>
-      <Typography variant="h6">Summary of Selections:</Typography>
-      <Box sx={{paddingTop: '10px'}}>
-        <Box sx={{display: 'flex', alignItems: 'center'}}>
-          <CalendarTodayIcon sx={{marginRight: '5px'}}/>
-          <span
-            style={{fontWeight: 'bold'}}>{startDate.format(startDate.year() === endDate.year() ? 'DD MMM' : 'DD MMM YY')} to {endDate.format('DD MMM YYYY')}</span>
-        </Box>
-        <Typography
-          sx={{paddingLeft: '32px'}}>{endDate.diff(startDate, 'days') + 1} days</Typography>
-      </Box>
-      <Box sx={{paddingTop: '10px'}}>
-        <Box sx={{display: 'flex', alignItems: 'center'}}>
-          <AccessTimeIcon sx={{marginRight: '5px'}}/>
-          <span style={{fontWeight: 'bold'}}>{startTime.format('HH:mm')} to {endTime.format('HH:mm')}</span>
-        </Box>
-        <Typography
-          sx={{paddingLeft: '32px'}}>{hours} hours {minutes !== 0 && ` and ${minutes} minutes`}</Typography>
-      </Box>
-      <Box sx={{paddingTop: '10px'}}>
-        <Box sx={{display: 'flex', alignItems: 'center'}}>
-          <PeopleIcon sx={{marginRight: '5px'}}/>
-          <span style={{fontWeight: 'bold'}}>{staffNumber} Staff</span>
-        </Box>
-      </Box>
-    </Box>
-    <Box sx={{paddingTop: '10px'}}>
-      <Button sx={{
-        textTransform: 'none',
-        paddingLeft: '10px',
-        color: 'white',
-        width: '100%',
-        backgroundColor: 'primary.main',
-        '&:hover': {
-          backgroundColor: 'primary.dark',
+  const timeOptions = timesBy15Minutes()
+
+  function timesBy15MinutesWithEnd(): string[] {
+    const times: string[] = [];
+    for (let hour = 0; hour < 24; hour++) {
+      for (let minute = 0; minute <= 45; minute += interval) {
+        const time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`
+        if (time !== '00:00') {
+          times.push(time);
         }
-      }} disabled={!!error} onClick={handleSubmitForm}>Save staff updates</Button>
-    </Box>
-  </Box>
+      }
+    }
+    times.push('00:00');
+    return times;
+  }
 
-}
+
+  const endTimeOptions = timesBy15MinutesWithEnd()
+
+  return (
+    <Box data-cy={`shift-staff-form`} sx={{padding: '10px 20px', width: '400px'}}>
+      <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+        <Typography variant="h2" component="h2" fontWeight={"bold"}>Edit staff</Typography>
+        <IconButton aria-label="close" color="inherit" size="small" onClick={cancelHandler}>
+          <CloseIcon fontSize="inherit"/>
+        </IconButton>
+      </Box>
+      <Box sx={{paddingTop: '10px'}}>
+        <Typography variant="h3" component="h3">Date</Typography>
+        <Box sx={{paddingTop: '10px'}}>
+          <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'en-gb'}>
+            <DatePicker data-cy="start-date-picker" sx={{backgroundColor: '#FFFFFF', width: '100%'}} label="Start Date" value={startDate}
+                        onChange={handleStartDateChange}
+                        format="DD MMMM YYYY"
+                        slots={{textField: (params) => <TextField {...params} data-cy="start-date-picker-text"/>}}
+            />
+          </LocalizationProvider>
+        </Box>
+        <Box sx={{paddingTop: '10px'}}>
+          <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={'en-gb'}>
+            <DatePicker data-cy="end-date-picker" sx={{backgroundColor: '#FFFFFF', width: '100%'}}
+                        label="End Date"
+                        value={endDate}
+                        onChange={handleEndDateChange}
+                        format="DD MMMM YYYY"
+                        slots={{textField: (params) => <TextField {...params} data-cy="end-date-picker-text"/>}}
+            />
+          </LocalizationProvider>
+        </Box>
+      </Box>
+      <Box sx={{paddingTop: '10px'}}>
+        <Typography variant="h3" component="h3">Time</Typography>
+        <Box sx={{paddingTop: '10px', display: 'flex', justifyContent: 'flex-start'}}>
+          <Box sx={{width: '50%', paddingRight: '10px'}}>
+            <Typography variant="h6">Start Time</Typography>
+            <Box>
+              <Select
+                variant="outlined"
+                value={startTime.format('HH:mm')}
+                onChange={(e) => {
+                  const [hour, minute] = e.target.value.split(':').map(Number);
+                  handleStartTimeChange(hour, minute);
+                }}
+                fullWidth
+                inputProps={{ role: 'start-time-select' }}
+                data-cy="start-time-select"
+              >
+                {timeOptions.map(time => (
+                  <MenuItem key={time} value={time} data-cy={`select-start-time-option-${time.replace(':', '-')}`}>{time} </MenuItem>
+                ))}
+              </Select>
+            </Box>
+          </Box>
+          <Box sx={{width: '50%', paddingLeft: '10px'}}>
+            <Typography variant="h6">End Time</Typography>
+            <Select
+              variant="outlined"
+              value={endTime.format('HH:mm')}
+              onChange={(e) => {
+                const [hour, minute] = e.target.value.split(':').map(Number);
+                handleEndTimeChange(hour, minute);
+              }}
+              fullWidth
+              inputProps={{ role: 'end-time-select' }}
+              data-cy="end-time-select"
+            >
+              {endTimeOptions.map(time => (
+                <MenuItem key={time} value={time} data-cy={`select-end-time-option-${time.replace(':', '-')}`}>{time}</MenuItem>
+              ))}
+            </Select>
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={{paddingTop: '10px', paddingBottom: '10px'}}>
+        <Typography variant="h3" component="h3">Staff</Typography>
+        <Box sx={{paddingTop: '10px'}}>
+          <TextField
+            label="Staff Number"
+            value={staffNumber}
+            onChange={handleStaffNumberChange}
+            type="number"
+            fullWidth
+            data-cy="staff-number-input"
+          />
+        </Box>
+      </Box>
+      {error && <Typography color="error" sx={{paddingTop: '10px'}}>{error}</Typography>}
+      <Box sx={{
+        paddingTop: '10px',
+        paddingBottom: '10px',
+        paddingLeft: '10px',
+        paddingRight: '20px',
+        backgroundColor: '#E6E9F1'
+      }}>
+        <Typography variant="h6">Summary of Selections:</Typography>
+        <Box sx={{paddingTop: '10px'}}>
+          <Box sx={{display: 'flex', alignItems: 'center'}}>
+            <CalendarTodayIcon sx={{marginRight: '5px'}}/>
+            <span
+              style={{fontWeight: 'bold'}}>{startDate.format(startDate.year() === endDate.year() ? 'DD MMM' : 'DD MMM YY')} to {endDate.format('DD MMM YYYY')}</span>
+          </Box>
+          <Typography sx={{paddingLeft: '32px'}}>{endDate.diff(startDate, 'days') + 1} days</Typography>
+        </Box>
+        <Box sx={{paddingTop: '10px'}}>
+          <Box sx={{display: 'flex', alignItems: 'center'}}>
+            <AccessTimeIcon sx={{marginRight: '5px'}}/>
+            <span style={{fontWeight: 'bold'}}>{startTime.format('HH:mm')} to {endTime.format('HH:mm')}</span>
+          </Box>
+          <Typography
+            sx={{paddingLeft: '32px'}}>{Math.floor(moment.duration(endTime.diff(startTime)).asHours())} hours {moment.duration(endTime.diff(startTime)).minutes()} minutes</Typography>
+        </Box>
+        <Box sx={{paddingTop: '10px'}}>
+          <Box sx={{display: 'flex', alignItems: 'center'}}>
+            <PeopleIcon sx={{marginRight: '5px'}}/>
+            <span style={{fontWeight: 'bold'}}>{staffNumber} Staff</span>
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={{paddingTop: '10px'}}>
+        <Button
+          sx={{
+            textTransform: 'none',
+            paddingLeft: '10px',
+            color: 'white',
+            width: '100%',
+            backgroundColor: 'primary.main',
+            '&:hover': {
+              backgroundColor: 'primary.dark',
+            }
+          }}
+          disabled={!!error}
+          onClick={handleSubmitForm}
+          data-cy="save-staff-button"
+        >
+          Save staff updates
+        </Button>
+      </Box>
+    </Box>
+  );
+};
