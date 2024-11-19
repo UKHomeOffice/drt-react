0775cda01ba64db261bba7eb7bb0f3e85891c7e1
jackson <Jackson Hyde>
2024-11-19 17:33:28 +0000

    Improving layout story

--- src/components/Pax/PaxSearchForm.stories.tsx
+++ src/components/Pax/PaxSearchForm.stories.tsx
@@ -3,8 +3,10 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from '@mui/x-date-pickers';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import InfoTooltip from '../ui/InfoTooltip';
+import {InfoTooltip} from '../ui/InfoTooltip';
 
+
+import { useArgs } from '@storybook/preview-api';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Box, TextField,Grid, ToggleButton, ToggleButtonGroup, Stack, Switch, FormControlLabel, FormLabel, Typography } from '@mui/material';
 import moment from 'moment';
@@ -23,6 +25,12 @@ const meta: Meta<PaxSearchFormStoryControls>  = {
     time: "now"
   },
   argTypes: {
+    timeMachine: {
+      options: [true, false],
+      control: {
+        type: 'radio'
+      }
+    },
     day: {
       options: ["yesterday", "today", "tomorrow"],
       control: {
@@ -42,7 +50,33 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const PaxFormLayout: Story = {
-  render: (storyContext) => {
+
+
+  render: () => {
+      const [args, updateArgs] = useArgs();
+
+      const handleChangeDay = ( event: React.MouseEvent<HTMLElement>, newValue: string | null) => {
+        updateArgs({
+          ...args,
+          day: newValue
+        });
+      };
+
+      const handleChangeTime = ( event: React.MouseEvent<HTMLElement>, newValue: string | null) => {
+        updateArgs({
+          ...args,
+          time: newValue
+        });
+      };
+
+      const handleChangeTimeMachine = ( event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event)
+        updateArgs({
+          ...args,
+          timeMachine: event.target.checked
+        });
+      };
+
       return (
       <LocalizationProvider dateAdapter={AdapterMoment}>
         <Box sx={(theme) => ({
@@ -53,12 +87,12 @@ export const PaxFormLayout: Story = {
           <Grid container spacing={2} flexWrap={'nowrap'}>
             <Grid item>
               <Stack spacing={2}>
-                <ToggleButtonGroup exclusive color='primary' size='medium' value={storyContext.day}>
+                <ToggleButtonGroup exclusive color='primary' size='medium' value={args.day} onChange={handleChangeDay}>
                   <ToggleButton value="yesterday" defaultChecked>Yesterday</ToggleButton>
                   <ToggleButton value="today">Today</ToggleButton>
                   <ToggleButton value="tomorrow">Tomorrow</ToggleButton>
                 </ToggleButtonGroup>
-                <ToggleButtonGroup exclusive color='primary' size='medium' value={storyContext.time}>
+                <ToggleButtonGroup exclusive color='primary' size='medium' value={args.time} onChange={handleChangeTime}>
                   <ToggleButton value="now">Now</ToggleButton>
                   <ToggleButton value="24hour">24 hours</ToggleButton>
                 </ToggleButtonGroup>
@@ -86,8 +120,8 @@ export const PaxFormLayout: Story = {
           <Stack direction="row" sx={{alignItems: 'center', mt: 1}}>
             <FormLabel htmlFor='time-machine' sx={{fontWeight: 'bold'}}>Time Machine</FormLabel>
             <InfoTooltip text='See what DRT was showing for this day on a specific date & time in the past. This can be useful to compare what DRT forecasted for a date compared to what ended up happening' />
-            <Switch id="time-machine" checked={storyContext.timeMachine} />
-            <Typography variant='body1'>{storyContext.timeMachine ? 'On' : 'Off'}</Typography>
+            <Switch id="time-machine" checked={args.timeMachine} onChange={handleChangeTimeMachine} />
+            <Typography variant='body1'>{args.timeMachine ? 'On' : 'Off'}</Typography>
           </Stack>
         </Box>
       </LocalizationProvider>
