aea6e0c0554d81664802e1212cd484dfa5410bc6
Nilesh Gupta <guptanilesh13@gmail.com>
2024-11-22 14:56:53 +0000

    adding drt theme
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- src/components/Footer/BottomBar.tsx
+++ src/components/Footer/BottomBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {AppBar, Link, Toolbar, Typography} from "@mui/material";
+import {AppBar, Link, ThemeProvider, Toolbar, Typography} from "@mui/material";
+import {drtTheme} from "../../index";
 
 export interface BottomBarProps {
   email: string;
@@ -9,23 +10,25 @@ export interface BottomBarProps {
 
 export const BottomBar = ({email, onClickAccessibilityStatement, url}: BottomBarProps) => {
   return (
-    <AppBar position="static" color="default" sx={{top: 'auto', bottom: 0}}>
-      <Toolbar sx={{
-        display: 'flex',
-        gap: '12px',
-        flexDirection: {xs: 'column', sm: 'row'},
-        alignItems: {xs: 'flex-start', sm: 'center'}
-      }}>
-        <Typography variant="body1">Support links:</Typography>
-        <Link href={`mailto:${email}`} target="_blank" underline="always">Email us</Link>
-        <Link underline="always" href="#accessibility/" onClick={() => {
-          onClickAccessibilityStatement();
+    <ThemeProvider theme={drtTheme}>
+      <AppBar position="static" color="default" sx={{top: 'auto', bottom: 0}}>
+        <Toolbar sx={{
+          display: 'flex',
+          gap: '12px',
+          flexDirection: {xs: 'column', sm: 'row'},
+          alignItems: {xs: 'flex-start', sm: 'center'}
         }}>
-          Accessibility statement
-        </Link>
-        <Link href={`${url}`} target="_blank" underline="always">Give feedback</Link>
-      </Toolbar>
-    </AppBar>
+          <Typography variant="body1">Support links:</Typography>
+          <Link href={`mailto:${email}`} target="_blank" underline="always">Email us</Link>
+          <Link underline="always" href="#accessibility/" onClick={() => {
+            onClickAccessibilityStatement();
+          }}>
+            Accessibility statement
+          </Link>
+          <Link href={`${url}`} target="_blank" underline="always">Give feedback</Link>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
