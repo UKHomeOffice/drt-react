4b4c6172a422330df25882cfbdaae7f6883b293b
Nilesh Gupta <guptanilesh13@gmail.com>
2024-11-25 10:14:20 +0000

    remove the explicit theme provider for bottom bar
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- src/components/Footer/BottomBar.tsx
+++ src/components/Footer/BottomBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {AppBar, Link, ThemeProvider, Toolbar, Typography} from "@mui/material";
-import {drtTheme} from "../../index";
+import {AppBar, Link, Toolbar, Typography} from "@mui/material";
 
 export interface BottomBarProps {
   email: string;
@@ -10,25 +9,23 @@ export interface BottomBarProps {
 
 export const BottomBar = ({email, onClickAccessibilityStatement, url}: BottomBarProps) => {
   return (
-    <ThemeProvider theme={drtTheme}>
-      <AppBar position="static" color="default" sx={{top: 'auto', bottom: 0}}>
-        <Toolbar sx={{
-          display: 'flex',
-          gap: '12px',
-          flexDirection: {xs: 'column', sm: 'row'},
-          alignItems: {xs: 'flex-start', sm: 'center'}
+    <AppBar position="static" color="default" sx={{top: 'auto', bottom: 0}}>
+      <Toolbar sx={{
+        display: 'flex',
+        gap: '12px',
+        flexDirection: {xs: 'column', sm: 'row'},
+        alignItems: {xs: 'flex-start', sm: 'center'}
+      }}>
+        <Typography variant="body1">Support links:</Typography>
+        <Link href={`mailto:${email}`} target="_blank" underline="always">Email us</Link>
+        <Link underline="always" href="#accessibility/" onClick={() => {
+          onClickAccessibilityStatement();
         }}>
-          <Typography variant="body1">Support links:</Typography>
-          <Link href={`mailto:${email}`} target="_blank" underline="always">Email us</Link>
-          <Link underline="always" href="#accessibility/" onClick={() => {
-            onClickAccessibilityStatement();
-          }}>
-            Accessibility statement
-          </Link>
-          <Link href={`${url}`} target="_blank" underline="always">Give feedback</Link>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+          Accessibility statement
+        </Link>
+        <Link href={`${url}`} target="_blank" underline="always">Give feedback</Link>
+      </Toolbar>
+    </AppBar>
   );
 };
 
