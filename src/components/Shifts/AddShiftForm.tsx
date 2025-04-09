ed27d93abb76bb14f0c3f8fc664d1d9e71744752
Rich Birch <ringo@albumsnaps.com>
2025-04-09 11:10:01 +0100

    DRTII-1741 Split shift form out to reduce complexity

--- src/components/Shifts/AddShiftForm.tsx
+++ src/components/Shifts/AddShiftForm.tsx
@@ -75,24 +75,8 @@ export const AddShiftForm = ({port, terminal, interval, shiftForms, confirmHandl
     isFirstRender.current = true;
   };
 
-  const handleChange = (id: number, field: keyof ShiftForm, value: any) => {
-    setTouched(true);
-    setShifts(shifts.map(shift => shift.id === id ? {...shift, [field]: value} : shift));
-  };
-
   const handleRemoveShift = (id: number) => {
     setShifts(shifts.filter(shift => shift.id !== id));
-  };
-
-
-  const handleStartTimeChange = (id: number, hour: number, minute: number) => {
-    const newStartTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
-    handleChange(id, 'startTime', newStartTime);
-  };
-
-  const handleEndTimeChange = (id: number, hour: number, minute: number) => {
-    const newEndTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
-    handleChange(id, 'endTime', newEndTime);
   };
 
   return (
@@ -103,107 +87,7 @@ export const AddShiftForm = ({port, terminal, interval, shiftForms, confirmHandl
             <Typography sx={{fontSize: '20px'}}>Add staff to {port} {getAirportNameByCode(port)} {terminal}</Typography>
             <Typography variant="h1" sx={{paddingBottom: '10px'}}>Step 1 of 2 - Create your shift pattern</Typography>
             {Array.from(shifts).map((shift) => (
-              <Box key={shift.id}
-                   sx={{mb: 2, p: 2, border: '1px solid #ccc', width: '300px', backgroundColor: '#E6E9F1'}}>
-                <Typography variant="h2" sx={{paddingBottom: '10px', fontSize: '24px'}}>Shift #{shift.id}</Typography>
-                {touched && error && (shift.name === '') && (
-                  <Typography color="error" variant="body2">Please add shift name</Typography>
-                )}
-                {touched && error && (shift.startTime === 'Select start time' || shift.startTime === '') && (
-                  <Typography color="error" variant="body2">Please select start time</Typography>
-                )}
-                {touched && error && (shift.endTime === 'Select end time' || shift.endTime === '') && (
-                  <Typography color="error" variant="body2">Please select end time</Typography>
-                )}
-                {touched && error && shift.startTime === shift.endTime && (
-                  <Typography color="error" variant="body2">Start time and end time cannot be the same</Typography>
-                )}
-                {shift.endTime < shift.startTime && (
-                  <Typography color="warning" variant="body2">This is a midnight shift spanning to the next day</Typography>
-                )}
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>Name of shift</Typography>
-                    <TextField
-                      fullWidth
-                      value={shift.name}
-                      placeholder="Enter the shift name"
-                      autoFocus
-                      onChange={(e) => handleChange(shift.id, 'name', e.target.value)}
-                      inputProps={{'data-cy': 'shift-name-input'}}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>Start time</Typography>
-                    <Box>
-                      <Select
-                        variant="outlined"
-                        value={timeOptionsForTheInterval.includes(shift.startTime) ? shift.startTime : "Select start time"}
-                        onChange={(e) => {
-                          const [hour, minute] = (e.target.value as string).split(':').map(Number);
-                          handleStartTimeChange(shift.id, hour, minute);
-                        }}
-                        fullWidth
-                        inputProps={{role: 'start-time-select'}}
-                        data-cy="start-time-select"
-                      >
-                        <MenuItem value="Select start time" disabled>
-                          Select start time
-                        </MenuItem>
-                        {timeOptionsForTheInterval.map(time => (
-                          <MenuItem key={time} value={time}
-                                    data-cy={`select-start-time-option-${time.replace(':', '-')}`}>
-                            {time}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>End time</Typography>
-                    <Select
-                      variant="outlined"
-                      value={timeOptionsForTheInterval.includes(shift.endTime) ? shift.endTime : "Select end time"}
-                      onChange={(e) => {
-                        const [hour, minute] = e.target.value.split(':').map(Number);
-                        handleEndTimeChange(shift.id, hour, minute);
-                      }}
-                      fullWidth
-                      inputProps={{role: 'end-time-select'}}
-                      data-cy="end-time-select"
-                    >
-                      <MenuItem value="Select end time" disabled>
-                        Select end time
-                      </MenuItem>
-                      {endTimeOptions(interval).map(time => (
-                        <MenuItem key={time} value={time}
-                                  data-cy={`select-end-time-option-${time.replace(':', '-')}`}>{time}</MenuItem>
-                      ))}
-                    </Select>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>Default staff number (optional)</Typography>
-                    <Grid item xs={12}>
-                      <Typography variant="body2" sx={{fontSize: '14px'}}>
-                        For current season only (change this at any time). It will only overwrite zero staffing in DRT.
-                      </Typography>
-                    </Grid>
-                    <TextField
-                      type="number"
-                      fullWidth
-                      value={shift.defaultStaffNumber}
-                      onChange={(e) => handleChange(shift.id, 'defaultStaffNumber', parseInt(e.target.value, 10))}
-                      inputProps={{'data-cy': 'staff-number-input'}}
-                    />
-                  </Grid>
 
-                  <Grid item xs={12}>
-                    <IconButton color="secondary" onClick={() => handleRemoveShift(shift.id)}>
-                      <CloseIcon/> <Typography sx={{textDecoration: 'underline'}}>Remove shift</Typography>
-                    </IconButton>
-                  </Grid>
-                </Grid>
-              </Box>
             ))}
             <Box>
               <Button variant="outlined" color="primary" onClick={handleAddShift} sx={{gap: 0, paddingLeft: '0'}} disabled={error || isFirstRender.current}>
@@ -235,3 +119,135 @@ export const AddShiftForm = ({port, terminal, interval, shiftForms, confirmHandl
   )
 };
 
+const EditShiftForm = (initialState: ShiftForm) => {
+
+  const [formState, setFormState] = useState<ShiftForm>(initialState);
+
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const handleStartTimeChange = (id: number, hour: number, minute: number) => {
+    const newStartTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+    setFormState((prev) => ({
+      ...prev,
+      startTime: newStartTime,
+    }));
+    if (formState.endTime !== '' && formState.endTime <= newStartTime) {
+      setErrors((prev) => [...prev, 'start-before-end']);
+    }
+    else setErrors(prev => prev.filter(error => error !== 'start-before-end'));
+  };
+
+  const handleEndTimeChange = (id: number, hour: number, minute: number) => {
+    const newEndTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+    setFormState((prev) => ({
+      ...prev,
+      endTime: newEndTime,
+    }));
+    if (formState.startTime !== '' && formState.startTime >= newEndTime) {
+      setErrors((prev) => [...prev, 'start-before-end']);
+    }
+    else setErrors(prev => prev.filter(error => error !== 'start-before-end'));
+  };
+
+  return <Box key={formState.id}
+              sx={{mb: 2, p: 2, border: '1px solid #ccc', width: '300px', backgroundColor: '#E6E9F1'}}>
+    <Typography variant="h2" sx={{paddingBottom: '10px', fontSize: '24px'}}>Shift #{formState.id}</Typography>
+    {errors.includes('name') && (
+      <Typography color="error" variant="body2">Please add shift name</Typography>
+    )}
+    {errors.includes('start-time') && (
+      <Typography color="error" variant="body2">Please select start time</Typography>
+    )}
+    {errors.includes('end-time') && (
+      <Typography color="error" variant="body2">Please select end time</Typography>
+    )}
+    {errors.includes('start-before-end') && (
+      <Typography color="error" variant="body2">Start time and end time cannot be the same</Typography>
+    )}
+    {formState.endTime < formState.startTime && (
+      <Typography color="warning" variant="body2">This is a midnight shift spanning to the next day</Typography>
+    )}
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>Name of shift</Typography>
+        <TextField
+          fullWidth
+          value={formState.name}
+          placeholder="Enter the shift name"
+          autoFocus
+          onChange={(e) => setFormState(prev => ({...prev, name: e.target.value}))}
+          inputProps={{'data-cy': 'shift-name-input'}}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>Start time</Typography>
+        <Box>
+          <Select
+            variant="outlined"
+            value={timeOptionsForTheInterval.includes(formState.startTime) ? formState.startTime : "Select start time"}
+            onChange={(e) => {
+              const [hour, minute] = (e.target.value as string).split(':').map(Number);
+              handleStartTimeChange(formState.id, hour, minute);
+            }}
+            fullWidth
+            inputProps={{role: 'start-time-select'}}
+            data-cy="start-time-select"
+          >
+            <MenuItem value="Select start time" disabled>
+              Select start time
+            </MenuItem>
+            {timeOptionsForTheInterval.map(time => (
+              <MenuItem key={time} value={time}
+                        data-cy={`select-start-time-option-${time.replace(':', '-')}`}>
+                {time}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>End time</Typography>
+        <Select
+          variant="outlined"
+          value={timeOptionsForTheInterval.includes(formState.endTime) ? formState.endTime : "Select end time"}
+          onChange={(e) => {
+            const [hour, minute] = e.target.value.split(':').map(Number);
+            handleEndTimeChange(formState.id, hour, minute);
+          }}
+          fullWidth
+          inputProps={{role: 'end-time-select'}}
+          data-cy="end-time-select"
+        >
+          <MenuItem value="Select end time" disabled>
+            Select end time
+          </MenuItem>
+          {endTimeOptions(interval).map(time => (
+            <MenuItem key={time} value={time}
+                      data-cy={`select-end-time-option-${time.replace(':', '-')}`}>{time}</MenuItem>
+          ))}
+        </Select>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography sx={{fontSize: '16px', fontWeight: 'bold'}}>Default staff number (optional)</Typography>
+        <Grid item xs={12}>
+          <Typography variant="body2" sx={{fontSize: '14px'}}>
+            For current season only (change this at any time). It will only overwrite zero staffing in DRT.
+          </Typography>
+        </Grid>
+        <TextField
+          type="number"
+          fullWidth
+          value={formState.defaultStaffNumber}
+          onChange={(e) => handleChange(formState.id, 'defaultStaffNumber', parseInt(e.target.value, 10))}
+          inputProps={{'data-cy': 'staff-number-input'}}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <IconButton color="secondary" onClick={() => handleRemoveShift(formState.id)}>
+          <CloseIcon/> <Typography sx={{textDecoration: 'underline'}}>Remove shift</Typography>
+        </IconButton>
+      </Grid>
+    </Grid>
+  </Box>
+}
