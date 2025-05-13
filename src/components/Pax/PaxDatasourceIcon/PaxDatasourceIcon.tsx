064a727a9c166b67caaa1d9f6818f9947d3489dc
Jackson Hyde <jacksonjhyde@gmail.com>
2025-05-13 14:10:21 +0100

    Upgrading PAX icons (#94)
    Upgrading pax datasource icons
    
    ---------
    
    Co-authored-by: Jackson Hyde <j.hyde@silex-consulting.com>

--- src/components/Pax/PaxDatasourceIcon/PaxDatasourceIcon.tsx
+++ src/components/Pax/PaxDatasourceIcon/PaxDatasourceIcon.tsx
@@ -1,27 +1,57 @@
 import React from "react";
 import DatasourceIcon from './DatasourceIcon.svg';
+import DatasourceIconGreen from './DatasourceIcon-green.svg';
+import DatasourceIconRed from './DatasourceIcon-red.svg';
+import DatasourceIconYellow from './DatasourceIcon-yellow.svg';
+import DatasourceIconBlack from './DatasourceIcon-black.svg';
 import { Tooltip, Box } from "@mui/material";
 
 import { DatasourceStatus, getPaxContent } from "../PaxUtils";
 
 export interface IPaxDatasource {
   status: DatasourceStatus,
+  width?: string,
 } 
 
-export const PaxDatasource = ({status}: IPaxDatasource) => {
+export const PaxDatasource = ({status, width}: IPaxDatasource) => {
   
   const content = getPaxContent(status);
 
+  let icon;
+  switch(status) {
+    case DatasourceStatus.Estimate:
+      icon = <DatasourceIconRed />;
+      break;
+    case DatasourceStatus.PortForecast:
+      icon = <DatasourceIconYellow />;
+      break;
+    case DatasourceStatus.DRTForecast:
+      icon = <DatasourceIconBlack />;
+      break;
+    case DatasourceStatus.PortLiveData:
+      icon = <DatasourceIconGreen />;
+      break;
+    case DatasourceStatus.CarrierData:
+      icon = <DatasourceIconBlack />;
+      break;
+    case DatasourceStatus.TerminalAverageData:
+      icon = <DatasourceIconRed />;
+      break;
+    case DatasourceStatus.PastCarrierData:
+      icon = <DatasourceIconYellow />;
+      break;
+    case DatasourceStatus.VerifiedCarrierData:
+      icon = <DatasourceIconGreen />;
+      break;
+    default:
+      icon = <DatasourceIcon />;
+      break;
+  }
+
   return (
-    <Tooltip title={`${content.statusText}`} sx={{cursor: 'pointer'}}>
-      <Box sx={{maxWidth: '18px', maxHeight: '24px', position: 'relative', marginTop: '-1px' }}>
-        <DatasourceIcon width={'20px'} />
-        { content.iconBg && <Box sx={{position: 'absolute', top: 3, left: 10}}>
-          { content.iconBg }
-        </Box>}
-        <Box sx={{position: 'absolute', top: 3, left: 10, textAlign: 'center'}}>
-          { content.icon }
-        </Box>
+    <Tooltip title={`${content.statusText}`}>
+      <Box sx={{width: width || '30px', cursor: 'pointer'}}>
+        { icon }
       </Box>
     </Tooltip>
   )
