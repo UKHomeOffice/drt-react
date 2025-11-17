afa53a95671809acdb519158eac4f9c248aa784b
Nilesh Gupta <guptanilesh13@gmail.com>
2025-11-17 10:22:53 +0000

    remove commented lines
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- src/components/Shifts/ConfirmRemoveShift.tsx
+++ src/components/Shifts/ConfirmRemoveShift.tsx
@@ -17,9 +17,9 @@ export const ConfirmRemoveShift = ({
 
   return (
     <Box sx={{p: 2, minWidth: '500px'}}>
-      <Typography variant="h1" sx={{paddingBottom: '10px'}}>Are you sure you want to remove this shift?</Typography>
+      <Typography variant="h1">Are you sure you want to remove this shift?</Typography>
           <Box key={shift.id}
-               sx={{maxWidth: '500px', backgroundColor: 'transparent', border: '1px solid #ddd', marginTop: '10px', marginBottom: '10px',}}>
+               sx={{maxWidth: '500px', backgroundColor: 'transparent', border: '1px solid #ddd', marginBottom: '10px',}}>
             <Box component="dl" sx={{
               display: 'flex',
               justifyContent: 'space-between',
@@ -51,12 +51,11 @@ export const ConfirmRemoveShift = ({
               <Box component="dd">{shiftDateToString(shift.startDate)}</Box>
             </Box>
           </Box>
-      {/*<Typography variant="body1">Are you sure you want to remove this shift?</Typography>*/}
-      <Box sx={{display: 'flex', justifyContent: 'flex-start', gap: '10px', marginTop: '20px'}}>
-        <Button variant="contained" color="primary" data-cy="shift-confirm-button"
-                onClick={() => onConfirm(shift)}>Remove</Button>
-      <Button variant="contained" color="secondary" data-cy="shift-confirm-button" onClick={() => onCancel()}>Cancel</Button>
-      </Box>
+          <Box sx={{display: 'flex', justifyContent: 'flex-start', gap: '10px', marginTop: '20px'}}>
+            <Button variant="contained" color="primary" data-cy="shift-confirm-button"
+                    onClick={() => onConfirm(shift)}>Remove</Button>
+          <Button variant="contained" color="secondary" data-cy="shift-confirm-button" onClick={() => onCancel()}>Cancel</Button>
+          </Box>
     </Box>
   )
 
